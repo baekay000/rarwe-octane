e8219b7aabb0c84e924e1b571c252e04bab2eead
kab08737 <kaylee.bae@kub.org>
2025-03-04 09:30:42 -0500

    pg 58: Add nested routes

--- app/router.js
+++ app/router.js
@@ -1,12 +1,13 @@
 import EmberRouter from '@ember/routing/router';
 import config from 'rarwe-octane/config/environment';
-
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
-
 Router.map(function () {
-  this.route('bands');
-  this.route('songs');
+  this.route('bands', function () {
+    this.route('band', { path: ':id' }, function () {
+      this.route('songs');
+    });
+  });
 });
