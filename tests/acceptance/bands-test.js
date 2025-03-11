333b49127cd8995a8f15416fd4114e8d28c2ae17
kab08737 <kaylee.bae@kub.org>
2025-03-11 11:21:42 -0400

    pg 215: Use Ember test selectors to test

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -1,8 +1,9 @@
 import { module, test } from 'qunit';
-import { visit, click, fillIn, waitFor } from '@ember/test-helpers';
+import { visit, waitFor } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { getPageTitle } from 'ember-page-title/test-support';
+import { createBand } from 'rarwe-octane/tests/helpers/custom-helpers';
 
 module('Acceptance | bands', function (hooks) {
   setupApplicationTest(hooks);
@@ -13,39 +14,35 @@ module('Acceptance | bands', function (hooks) {
     this.server.create('band', { name: 'Long Distance Calling' });
     await visit('/');
     assert.equal(getPageTitle(), 'Bands | Rock & Roll with Octane');
-    let bandLinks = document.querySelectorAll('.mb-2 > a');
-    assert.equal(bandLinks.length, 2, 'All band links are rendered');
-    assert.ok(
-      bandLinks[0].textContent.includes('Radiohead'),
-      'First band link contains the band name'
-    );
-    assert.ok(
-      bandLinks[1].textContent.includes('Long Distance Calling'),
-      'The other band link contains the band name'
-    );
+    assert
+      .dom('[data-test-rr="band-link"]')
+      .exists({ count: 2 }, 'All band links are rendered');
+    assert
+      .dom('[data-test-rr="band-list-item"]:first-child')
+      .hasText('Radiohead', 'The first band link contains the band name');
+    assert
+      .dom('[data-test-rr="band-list-item"]:last-child')
+      .hasText(
+        'Long Distance Calling',
+        'The other band link contains the band name'
+      );
   });
 
   test('Create a band', async function (assert) {
     this.server.create('band', { name: 'Royal Blood' });
 
     await visit('/');
-    await click('[data-test-rr="new-band-button"]');
-    await fillIn('[data-test-rr="new-band-name"]', 'Caspian');
-    await click('[data-test-rr="save-band-button"]');
+    await createBand('Caspian');
     await waitFor('[data-test-rr="no-songs-text"]');
 
-    let bandLinks = document.querySelectorAll('[data-test-rr="band-link"]');
-    assert.equal(bandLinks.length, 2, 'A new band link is rendered');
-
-    assert.ok(
-      bandLinks[1].textContent.includes('Caspian'),
-      'The new band link is rendered as the last item'
-    );
-    assert.ok(
-      document
-        .querySelector('[data-test-rr="songs-nav-item"]')
-        .textContent.includes('Songs'),
-      'The Songs tab is active'
-    );
+    assert
+      .dom('[data-test-rr="band-list-item"]')
+      .exists({ count: 2 }, 'A new band link is rendered');
+    assert
+      .dom('[data-test-rr="band-list-item"]:last-child')
+      .hasText('Caspian', 'The new band link is rendered as the last item');
+    assert
+      .dom('[data-test-rr="songs-nav-item"] > .active')
+      .exists('The Songs tab is active');
   });
 });
