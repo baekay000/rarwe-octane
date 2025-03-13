7d9bd15270e9baaa88075f1b3eecd2f589e2be4b
kab08737 <kaylee.bae@kub.org>
2025-03-13 09:36:40 -0400

    pg 269: Add error and loading substates

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,10 +1,9 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-
 export default class BandsRoute extends Route {
   @service catalog;
 
-  model() {
+  async model() {
     return this.catalog.fetchAll('bands');
   }
 }
